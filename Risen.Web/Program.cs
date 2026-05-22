ebcc55c8e1f4446201101152c9bcf538707278be
JavidGasim <gasimovcavid777@gmail.com>
2026-05-23 00:59:57 +0400

    jwtbearer for signalr updated

--- Risen.Web/Program.cs
+++ Risen.Web/Program.cs
@@ -80,6 +80,24 @@ builder.Services
             ValidateLifetime = true,
             ClockSkew = TimeSpan.FromSeconds(30)
         };
+
+        // 🔥 THIS IS REQUIRED FOR SIGNALR
+        opt.Events = new JwtBearerEvents
+        {
+            OnMessageReceived = context =>
+            {
+                var accessToken = context.Request.Query["access_token"];
+                var path = context.HttpContext.Request.Path;
+
+                if (!string.IsNullOrEmpty(accessToken) &&
+                    path.StartsWithSegments("/notificationHub"))
+                {
+                    context.Token = accessToken;
+                }
+
+                return Task.CompletedTask;
+            }
+        };
     });
 
 
